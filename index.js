532f0a48f2f252d05e6fea7d3b1bf048fac238d6
Nathan M <nathanmasol@gmail.com>
2019-04-12 08:56:52 -0500

    Archivo se crea por cada login

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ var express = require('express');
 var exphbs = require('express-handlebars');
 
 var app = express();
+var cont =0;
 
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
+
+const fs = require('fs');
 
 app.get('/', function(request,response){
     var context={
@@ -16,9 +19,22 @@ app.get('/', function(request,response){
     response.render('home',context);
 });
 
+app.get('/bienvenida', function(request,response){
+    var context={
+        titulo: 'Hola :D',
+    }
+    response.render('bienvenida',context);
+});
+
+function archivoEscrito(){
+    console.log('se creeo el archivo xdxdxd');
+}
+
 app.post('/login', function(request,response){
     console.log(request.body);
-    response.send('hola');
+    cont ++;
+    fs.writeFile('info'+cont+'.txt', 'Correo: '+ request.body.correo+'/ Contrasena:'+ request.body.contrasena, 'utf8', archivoEscrito);
+    response.redirect('/bienvenida');
 });
 
 app.listen(3000);
